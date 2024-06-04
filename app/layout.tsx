c8469ec0f74809119c46a21d0d8e25af7a7213f0
Abhinav Raghunandan <raghunandan.abhinav@gmail.com>
2024-06-04 14:41:32 +0530

    Integrated clerk user management system for easy auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
+import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+
 const IBMPlex = IBM_Plex_Sans({ 
   subsets: ["latin"],
   weight: ['400', '500', '600', '700'],
@@ -20,10 +22,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: { colorPrimary: '#624cf5' }
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+              {children}             
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
